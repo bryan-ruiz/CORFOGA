f71be6111bd5de10cdf8619c9f9b2bd50323b5f5
AdrianHS <ad1190@hotmail.com>
2018-03-19 13:46:18 -0600

    Cambios vistas
    Cambios vistas

--- CORFOGA/app/src/main/java/com/example/bryan/corfoga/LoginActivity.java
+++ CORFOGA/app/src/main/java/com/example/bryan/corfoga/LoginActivity.java
@@ -2,12 +2,28 @@ package com.example.bryan.corfoga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button b1;
 
+    private ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        b1=(Button)findViewById(R.id.login);
+        spinner=(ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
